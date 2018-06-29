83a8c8f48f529b14aa9fcd1e3b479a822f190d15
William <williamsrz@outlook.com>
2018-06-29 20:44:51 -0300

    teste

---
+++ AwesomeSeries/AwesomeSeries/AwesomeSeries/ViewModel/Base/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AwesomeSeries.ViewModel.Base
+{
+    public abstract class ViewModelBase : BindableObject
+    {
+        string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; OnPropertyChanged(); }
+        }
+
+        public ViewModelBase(string title)
+        {
+            Title = title;
+        }
+
+        public virtual Task InitializeAsync(object navgationData)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ AwesomeSeries/AwesomeSeries/AwesomeSeries/ViewModel/Base/ViewModelLocator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeSeries.ViewModel.Base
+{
+    public class ViewModelLocator
+    {
+
+    }
+}
