5b9fcb0ff4ed034cd18949bacb286095254de66e
Vanderlei dos Santos Teles Junior <vanderleitelesjr@hotmail.com>
2018-07-02 10:35:30 -0300

    Icon

--- AwesomeSeries/AwesomeSeries/AwesomeSeries.Android/MainActivity.cs
+++ AwesomeSeries/AwesomeSeries/AwesomeSeries.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace AwesomeSeries.Droid
 {
-    [Activity(Label = "AwesomeSeries", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "AwesomeSeries", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
